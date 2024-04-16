9efdbb4aeabd556004c21a2380ed264a2f37bc22
Serhii Bondar <sergiibondar1979@gmail.com>
2024-04-16 16:28:19 +0300

    fixed methods of CalculatorTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
 
     @Test
     public void testAdd(){
-        var calculator = new Calculator();
+        Calculator calculator = new Calculator();
         assertEquals(8, calculator.add(3, 5));
         assertEquals(1, calculator.add(0, 1));
         assertEquals(0, calculator.add(-2, 2));
@@ -16,14 +16,14 @@ public class CalculatorTest {
 
     @Test
     public void testSubtract(){
-        var calculator = new Calculator();
+        Calculator calculator = new Calculator();
         assertEquals(2, calculator.subtract(5, 3));
         assertEquals(-2, calculator.subtract(3, 5));
     }
 
     @Test
     public void testMultiply(){
-        var calculator = new Calculator();
+        Calculator calculator = new Calculator();
         assertEquals(4, calculator.multiply(2, 2));
         assertEquals(-4, calculator.multiply(-2, 2));
         assertEquals(4, calculator.multiply(-2, -2));
@@ -32,7 +32,7 @@ public class CalculatorTest {
 
     @Test
     public void testDivide(){
-        var calculator = new Calculator();
+        Calculator calculator = new Calculator();
         assertEquals(2.0, calculator.divide(8, 4), 0.001);
         assertEquals(-2.0, calculator.divide(-6, 3), 0.001);
         assertEquals(0.5, calculator.divide(2, 4), 0.001);
@@ -40,7 +40,7 @@ public class CalculatorTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testDivideByZero(){
-        var calculator = new Calculator();
+        Calculator calculator = new Calculator();
         calculator.divide(2, 0);
     }
 }
